c1847b09babbef344b2043b575fc81fed5809725
ikumen <ikumen@gnoht.com>
2020-12-27 23:58:52 -0800

    Today's solution

--- advent-of-code/aoc-2020/03.py
+++ advent-of-code/aoc-2020/03.py
@@ -8,12 +8,31 @@ Thoughts:
 """
 import helpers
 
+def count_trees_in_path(matrix, start, slope):
+	"""Given a slice of a larger repeating matrix, and a slope for 
+	a path through the matrix, count the number of trees in the path.
+
+	:param matrix: 2D array representing slice of larger matrix
+	:param start: tuple of starting position
+	:param slope: tuple of run/rise (right and down)
+	"""
+	trees = 0
+	bottom = len(matrix)
+	x = start[0] + slope[0]
+	y = start[1] + slope[1]
+	while y < bottom:
+		if matrix[y][x]:
+			trees += 1
+		x = (x + slope[0]) % len(matrix[y])
+		y += slope[1]
+	return trees
+
 def main():	
 	matrix = []
 	with open(helpers.get_data_file_path(__file__)) as reader:
 		for line in reader.readlines():
 			matrix.append([c == '#' for c in line.strip()])
-	print(matrix)
+	print(count_trees_in_path(matrix, [0,0], [3,1]))
 
 
 if __name__ == "__main__":
